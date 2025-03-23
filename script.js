330c542b431c07baa926fdad3588e759869df3aa
Bladevik <Blade-85@inbox.ru>
2025-03-23 12:36:59 +0300

    модалка обратной связи

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const callbackButton = document.querySelector('.callback');
+const modal = document.querySelector('#callbackModal');
+const closeButton = document.querySelector('.close-button');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+callbackButton.addEventListener('click', () => {
+    modal.style.display = 'flex'; // Показываем модальное окно
+});
+
+closeButton.addEventListener('click', () => {
+    modal.style.display = 'none'; // Скрываем модальное окно
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none'; // Скрываем при клике вне формы
+    }
 });
